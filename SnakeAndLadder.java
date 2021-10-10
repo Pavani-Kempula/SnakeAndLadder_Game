a076c6d32cda33bbbf16c52a235c2e9020467684
PavaniKempula <pavanikempula1998@gmail.com>
2021-10-10 13:13:21 +0530

    MODIFIED : Taking a note of how many times dice has been rolled and at the end of the program printing the count

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -12,61 +12,69 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		
 		// Displaying Welcome Message
-				System.out.println("Welcome To Snake And Ladder Game");
+		System.out.println("Welcome To Snake And Ladder Game");
 
-				int position = 0;
-				int reach = 0;
-				int roll = 0;
-				int start = 0;
+		int position = 0;
+		int reach = 0;
+		int roll = 0;
+		int start = 0;
+		int counter = 0;
 
-				while (reach < WINNING_POSITION) {
-					reach++;
-					/*
-					 * Using random to get Dice number
-					 * Math.random() function generating random numbers from range 1 to 6
-					 * Converting it to nearest possible integer number using Math.floor()
-					 */
-					int diceValue = (int) Math.floor(Math.random() * 6) + 1;
-					System.out.println("Dice value is :" + diceValue);
-					
-					// Using random to check for option of No Play, Snake or Ladder
-					int option = (int) Math.floor(Math.random() * 10) % 3;
-					System.out.println("Option is :" + option);
-					
-					//Repeating till the Player reaches the Exact Winning Position
-					switch (option) {
-					case NO_PLAY:
-						roll = 0;
-						break;
-					case LADDER:
-						roll = +diceValue;
-						break;
-					case SNAKE:
-						roll = -diceValue;
-						break;
-					}
-
-					position = (position + roll);
-					if ((position < start) && (option == 0)) {
-						System.out.println("Current Position: " + start);
-						reach = start;
-						System.out.println("Reached: " + reach);
-					} else if ((position > WINNING_POSITION) && (option == 1)) {
-						System.out.println("Current Position: " + position);
-						reach = WINNING_POSITION;
-						System.out.println("Reached: " + reach);
-						break;
-					} else if (position < 0) {
-						System.out.println("Current Position: " + start);
-						reach = start;
-						System.out.println("Reached: " + reach);
-					} else {
-						System.out.println("Current Position: " + position);
-						reach = position;
-						System.out.println("Reached: " + reach);
-					}
-				}
-				System.out.println("Win:" + reach);
+		while (reach < WINNING_POSITION) {
+			reach++;
+			
+			/*
+			 * Using random to get Dice number
+			 * Math.random() function generating random numbers from range 1 to 6
+			 * Converting it to nearest possible integer number using Math.floor()
+			 */
+			int diceValue = (int) Math.floor(Math.random() * 6) + 1;
+			System.out.println("Dice value is :" + diceValue);
+			
+			// Using random to check for option of No Play, Snake or Ladder
+			int option = (int) Math.floor(Math.random() * 10) % 3;
+			System.out.println("Option is :" + option);
+			
+			switch (option) {
+			case NO_PLAY:
+				roll = 0;
+				break;
+			case LADDER:
+				counter++;
+				roll = +diceValue;
+				break;
+			case SNAKE:
+				roll = -diceValue;
+				break;
+			}
+			
+			/*
+			 * Repeating till the Player reaches the winning position 100
+			 * Reporting the number of times the dice was played to win 
+			 * Also Showing Current Position after every dice roll
+			 */
+			position = (position + roll);
+			if ((position < start) && (option == 0)) {
+				System.out.println("Current Position: " + start);
+				reach = start;
+				System.out.println("Reached: " + reach);
+			} else if ((position > WINNING_POSITION) && ((option == 1) || (option == 2))) {
+				System.out.println("Current Position: " + position);
+				reach = WINNING_POSITION;
+				System.out.println("Reached: " + reach);
+				break;
+			} else if (position < 0) {
+				System.out.println("Current Position: " + start);
+				reach = start;
+				System.out.println("Reached: " + reach);
+			} else {
+				System.out.println("Current Position: " + position);
+				reach = position;
+				System.out.println("Reached: " + reach);
+			}
+		}
+		System.out.println("Number of time dice was rolled to win game :" + counter);
+		System.out.println("Win:" + reach);
 	}
 
 }
